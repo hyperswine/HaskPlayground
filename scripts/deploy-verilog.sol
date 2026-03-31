6e373f26814d4eac29ca0f878c31dac2b81add77
Jasen Qin <jasen.qin33@gmail.com>
2026-03-31 18:31:44 +1100

    Script: use new sol scripts

--- scripts/deploy-verilog.sol
+++ scripts/deploy-verilog.sol
@@ -1,9 +1,8 @@
 #!/usr/bin/env sol
 # Copies the Clash-generated top.v and prog.hex into examples/processor-verilog.
 
-repo_root = sh "git rev-parse --show-toplevel" |> unwrap_or ".".
-src = "{repo_root}/verilog/Processor.topEntity3".
-dest = "{repo_root}/examples/processor-verilog".
+src = "verilog/Processor.topEntity3".
+dest = "examples/processor-verilog".
 
 result1 = cp "{src}/top.v" "{dest}/top.v".
 echo result1.
