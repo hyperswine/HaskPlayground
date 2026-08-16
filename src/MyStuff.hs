5fc88173e24b5a41e26b83dc6b00238b03c3d700
Jason Qin <capitalswine@gmail.com>
2026-08-16 20:36:23 +1000

    MyStuff: File !

---
+++ src/MyStuff.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module MyStuff where
+
+import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Lazy as L
+import Data.Char (isSpace)
+
+data Greymap = Greymap {greyWidth :: Int, greyHeight :: Int, greyMax :: Int, greyData :: L.ByteString} deriving (Eq, Show)
+
+matchHeader :: L.ByteString -> L.ByteString -> Maybe L.ByteString
+
+getNat :: L.ByteString -> Maybe (Int, L.ByteString)
+
+getBytes :: Int -> L.ByteString -> Maybe (L.ByteString, L.ByteString)
+
+-- 1 so it doesnt beat $
+infixl 1 |>
+a |> b = b a
+
+-- matchHeader prefix str | prefix `L8.isPrefixOf` str = Just $ L8.dropWhile isSpace $ L.drop (L.length prefix) str
+matchHeader prefix str | prefix `L8.isPrefixOf` str = Just $ (L.length prefix) |> flip L.drop str |> L8.dropWhile isSpace
+matchHeader _ _ = Nothing
+
+b1 :: L.ByteString
+b1 = "hi"
+
+b2 :: L.ByteString
+b2 = "hihi"
+
+b3 :: L.ByteString
+b3 = "h"
+
+getNat s | Just (num, rest) <- L8.readInt s = if num <= 0 then Nothing else Just (fromIntegral num, rest)
+getNat _ = Nothing
+
+getBytes n str = let count = fromIntegral n
+                     both@(prefix, _) = L.splitAt count str
+                  in if L.length prefix < count then Nothing else Just both
+
+-- does not say anything about consumption, but matcheHeader does consume
+Nothing >>? _ = Nothing
+Just v >>? f = f v
+
+-- the >>? is a Just/Nothing to terminate early, railway oriented programming without having to define a failure mode each time due to common mode
+-- but does not have extra state, have to do it yourself by returning (a, b) and matching on \(x, y)
+
+skipSpace (a, s) = Just (a, L8.dropWhile isSpace s)
+
+parseP5 :: L.ByteString -> Maybe (Greymap, L.ByteString)
+parseP5 s =
+  matchHeader (L8.pack "P5") s  -- if it fails here, it will do Nothing >> _ = Nothing
+  >>? \s -> skipSpace ((), s) -- instead of one element, return a pair
+  >>? (getNat . snd) -- get snd element, then apply getNat to it (the width)
+  >>? skipSpace -- optional, can just not see any spaces and the next char and keep going with Just rest
+  >>? \(width, s) -> getNat s -- match a pair involving the width, then try to get a nat (height)
+  >>? skipSpace
+  >>? \(height, s) -> getNat s
+  >>? \(maxGrey, s) -> getBytes 1 s
+  >>? (getBytes (width * height) . snd)
+  >>? \(bitmap, s) -> Just (Greymap width height maxGrey bitmap, s)
